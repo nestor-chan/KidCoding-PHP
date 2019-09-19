5529e6d3ac3eaa699c94056e55144ebf80ba8698
root <root@ip-172-31-87-171.ec2.internal>
2019-09-19 19:48:32 +0000

    add hello.php

---
+++ examples/hello.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "";
+echo '<span style="color:#AFA;text-align:center;">Request has been sent. Please wait for my reply!</span>';
+
+
+?>
+
